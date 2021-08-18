b38b7b920bac8ef8abb791b4b79a32b27c5dddd7
Marcos Rossetto <marcosrst23@gmail.com>
2021-08-18 10:41:34 -0300

    :heavy_plus_sign: styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import dark from './styles/themes/dark';
+
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={dark}>
+      <div className="App">
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
