bec1ead0260f94c15e6aa619e4f4cf01b468623d
Marcos Rossetto <marcosrst23@gmail.com>
2021-08-18 16:37:15 -0300

    :sparkles: add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { ThemeProvider } from 'styled-components';
 import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Search from './components/Search';
+import Menu from './components/Menu';
+import List from './components/List';
 
 function App() {
   return (
     <ThemeProvider theme={dark}>
       <div className="App">
         <GlobalStyle />
+        <Header />
+        <Search />
+        <List />
+        <Menu />
       </div>
     </ThemeProvider>
   );
