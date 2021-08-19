8ce1aff4ceb25a3dde71ea57946b9a690234fe86
Marcos Rossetto <marcosrst23@gmail.com>
2021-08-19 17:02:33 -0300

    :sparkles: add deezer api

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
-import ListItem from '../ListItem';
+import React, { useEffect, useState } from 'react';
+import ListItem, { Track } from '../ListItem';
+import api from '../../services/api';
 
 import { Container } from './styles';
+import { secondToMinute } from '../../utils/secondsToMinutes';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Search from '../Search';
 
 const List: React.FC = () => {
+  const [track, setTrack] = useState<Track[]>([]);
+  const [index, setIndex] = useState(0);
+  let limit = 100;
+
+  async function getTrack() {
+    try {
+      const response = await api.get(
+        `chart/0/tracks?index=${index}&limit=${limit}`
+      );
+
+      const formattedResult = await response.data.data.map((data: any) => {
+        return {
+          ...data,
+          duration: secondToMinute(data.duration),
+        };
+      });
+
+      setTrack([...track, ...formattedResult]);
+
+      if (index === 0) return setIndex(index + limit);
+      if (index > 0) return setIndex(index * 2);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTrack();
+  }, []);
+
   return (
-    <Container>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+    <Container id="scrollableDiv">
+      <Search />
+      <InfiniteScroll
+        dataLength={track.length - 5} //This is important field to render the next data
+        next={getTrack}
+        hasMore={true}
+        loader={<p></p>}
+        endMessage={<p></p>}
+        scrollableTarget="scrollableDiv"
+      >
+        {track.map((item: Track) => {
+          return <ListItem key={item.id} track={item} />;
+        })}
+      </InfiniteScroll>
     </Container>
   );
 };
