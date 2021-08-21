6bcc3200ac9265af7207548728cb761850f2ed0f
Marcos Rossetto <marcosrst23@gmail.com>
2021-08-21 13:04:53 -0300

    :recycle: add baseURL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  headers: {
-    apiId: process.env.APP_ID,
-  },
-  baseURL: 'https://api.deezer.com/',
+  baseURL: 'http://api.deezer.com/',
 });
 
 export default api;
